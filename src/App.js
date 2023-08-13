0a3e85151741af062029d3e066ca08d7cbac0bf6
nishanthmony <nishanth19007shady@gmail.com>
2023-08-13 20:59:19 +0530

    count button in vertical

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
 
 function App() {
+  const [text, count] = useState(0)
+  const incval=() => {
+    count(text+1)
+  }
+  const decval = () => {
+    count(text-1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>FRUIT - O - LICIOUS</h1>
+      <button onClick={incval} className='incButton'>+</button>
+      <h2>{text}</h2>
+      <button onClick={decval} className='decButton'>-</button>
     </div>
   );
 }
