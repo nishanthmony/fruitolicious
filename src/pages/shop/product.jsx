dd86fd2622d4606ad8488481214eaf6f7040db55
nishanthmony <nishanth19007shady@gmail.com>
2023-08-24 10:50:42 +0530

    heading

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -1,17 +1,9 @@
 import React, { useContext } from 'react'
-import {useState} from 'react'
 import {ShopContext} from '../../context/shop-context'
 
 export const Product = (props) => {
     const { prodId, prodName, prodPrice, prodImg } = props.data;
     const {addToCart, cartItems} = useContext(ShopContext);
-    const [text, count] = useState(0)
-    const incval=() => {
-      count(text+1)
-    }
-    const decval = () => {
-      count(text-1)
-    }
   return (
     <div className='product'>
         <img src={prodImg} alt=''/>
